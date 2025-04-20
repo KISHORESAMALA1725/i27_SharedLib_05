92d5bbdd49cb9edd88165081fb067d5a5c751e58
kishore <kishore.9686@gmail.com>
2025-04-20 06:53:53 +0530

    auth_login

---
+++ src/com/i27academy/k8s/k8s.groovy
@@ -0,0 +1,18 @@
+package com.i27academy.k8s;
+
+class k8s {
+    def jenkins
+    k8s(jenkins) {
+        this.jenkins = jenkins      
+    }
+
+    def auth_login(clusterName, zone, projectID) {
+        jenkins.sh """
+        echo "Entering K8S authentication/login methods"
+        gcloud compute instances list
+        echo "Create config file for env"
+        gcloud container clusters get-credentials i27-${clusterName}} --${zone} us-central1-c --project ${projectID}
+        """
+    }
+}
+
