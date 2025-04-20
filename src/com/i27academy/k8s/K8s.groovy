ff596f9e6a207e8f4ff444080cafc81b43d07499
kishore <kishore.9686@gmail.com>
2025-04-20 08:53:16 +0530

    cd4567

--- src/com/i27academy/k8s/K8s.groovy
+++ src/com/i27academy/k8s/K8s.groovy
@@ -12,7 +12,7 @@ class K8s {
             echo "Entering K8S authentication/login methods"
             gcloud compute instances list
             echo "Create config file for env"
-            gcloud container clusters get-credentials ${clusterName} --${zone} us-central1-c --project ${projectID}
+            gcloud container clusters get-credentials $clusterName --$zone us-central1-c --project $projectID
             kubectl get nodes
         """
     }
